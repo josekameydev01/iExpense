6e9f93e85ae7abf990578f82f98a3f683b24ce2b
josekameydev01 <gk01cracklitos@gmail.com>
2025-10-06 21:51:59 -0600

    first commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by carlosgalvankamey on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
