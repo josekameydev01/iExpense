aa0f0b55f0c6b052346871ea19877b4c63460993
josekameydev01 <gk01cracklitos@gmail.com>
2025-10-22 16:52:22 -0600

    showing and hiding views

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Observation
 
 @Observable
 class User {
@@ -17,10 +16,39 @@ class User {
         self.firstName = firstName
         self.lastName = lastName
     }
+    
+    init() {
+        self.firstName = ""
+        self.lastName = ""
+    }
+}
+
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    let name: String
+    let lastName: String
+    
+    var body: some View {
+        Spacer()
+        Group {
+            Text("Welcome")
+            Text("\(name) \(lastName)")
+        }
+        .font(.largeTitle)
+        .fontWeight(.bold)
+        
+        Button("Dismiss") {
+            dismiss()
+        }
+        .buttonStyle(.borderedProminent)
+        Spacer()
+    }
 }
 
 struct ContentView: View {
-    @State private var user = User(firstName: "", lastName: "")
+    @State private var user = User()
+    @State private var showingSheet = false
     var body: some View {
         VStack {
             Text("iExpense")
@@ -48,17 +76,20 @@ struct ContentView: View {
             
             Spacer()
             
-            VStack(spacing: 10) {
-                Text("First name: \(user.firstName)")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Text("Last name: \(user.lastName)")
-                    .frame(maxWidth: .infinity, alignment: .leading)
+            Button("Show Sheet") {
+                if !user.firstName.isEmpty && !user.lastName.isEmpty {
+                    showingSheet.toggle()
+                }
             }
-            .padding()
+            .buttonStyle(.borderedProminent)
+            
             Spacer()
             
         }
         .frame(maxHeight: .infinity)
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: user.firstName, lastName: user.lastName)
+        }
     }
 }
 
